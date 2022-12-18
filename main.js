9eee5e0b601c0aee685568f0cd20255565fecbcd
dtriana <dstm1801@gmail.com>
2022-12-17 21:45:43 -0500

    Completed showing and hiding images functionallity

--- main.js
+++ main.js
@@ -3,3 +3,13 @@ import {ToggleButton} from "./components/ToggleButton.js"
 
 window.customElements.define("toggle-button", ToggleButton);
 window.customElements.define("nft-card", NFTCard);
+
+const hiddeShowSwitch = document.querySelector("toggle-button");
+hiddeShowSwitch.addEventListener("change", () => {
+    const styleSheet = document.styleSheets[0];
+    if (hiddeShowSwitch.checked) {
+        styleSheet.insertRule("nft-card {--img-filter: blur(15px); --eye-opacity: 1}", 0);
+    } else {
+        styleSheet.deleteRule(0);
+    }
+});
